17fec6767f422794b4c8c8afed8c1f10c8da9c6a
wellitsabhi <abhishekongit@gmail.com>
2024-03-01 19:25:38 +0530

    signin done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,22 @@
 const {Router} = require('express');
-const User = require('../models/user.models');
+const User = require('../models/user.models.js');
 
 const router = Router();
 
-router.get('/signin', (req, res) => {
+router.get('/signin',  (req, res) => {
     return res.render('signin');
 })
 
 router.get('/signup', (req, res) => {
     return res.render('signup');
+})
+
+router.post('/signin', async(req, res) => {
+    const {email, password} =req.body;
+    const user =  await User.matchPassword(email,password);
+
+    console.log('User',user);
+    return res.redirect('/');
 })
 
 router.post('/signup', async(req, res) => {
