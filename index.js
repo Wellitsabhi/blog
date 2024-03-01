fcbddb2661686e4f7675b85308fb924c01e3b672
wellitsabhi <abhishekongit@gmail.com>
2024-03-01 16:31:24 +0530

    signup code

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 8000;
 
-app.set("view engine", "ejs")
-app.set("views", path.resolve("./views"))
+// Connect to  mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/blogger')
+.then( console.log('MongoDB Connected'))
 
+// Routes
+const userRoute = require('./routes/user.routes.js');
 
-app.get("/", (req, res) => {
+// ejs
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./views'))
+
+// middleare to handle form data
+app.use(express.urlencoded({ extended: false}))
+
+app.get('/', (req, res) => {
     res.render("home")
 })
+
+// Middleware
+app.use("/user", userRoute);
+
 app.listen(PORT, ()=>{
     console.log(`App listening on ${PORT}`);
 });
